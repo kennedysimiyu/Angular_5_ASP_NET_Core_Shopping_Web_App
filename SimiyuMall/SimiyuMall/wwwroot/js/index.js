b992182c372f086fea9ef2b8cb71a5386f02e071
Ken Simiyu <kensimiyu2015@gmail.com>
2018-02-10 11:03:28 +0300

    adding javascript hidding form and alert functionalities

---
+++ SimiyuMall/SimiyuMall/wwwroot/js/index.js
@@ -0,0 +1,14 @@
+﻿var x = 0;
+
+var s = "";
+
+console.log("Hello Customer");
+
+var contactForm = document.getElementById("contactForm");
+    contactForm.hidden = true;
+
+    var button = document.getElementById("byButton");  
+
+    button.addEventListener("click", function () {
+    alert("Buying Item");
+    });
