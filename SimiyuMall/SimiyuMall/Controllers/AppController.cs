7a2cf541bcdcf068d4094932f345ab1e40d8f389
Ken Simiyu <kensimiyu2015@gmail.com>
2018-02-10 15:10:03 +0300

    adding an mvc.net razor view

--- SimiyuMall/SimiyuMall/Controllers/AppController.cs
+++ SimiyuMall/SimiyuMall/Controllers/AppController.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SimiyuMall.Controllers
 {
-    public class AppController
+    // our controller derives from a class in mvc called Controller
+    public class AppController : Controller
     {
+    // action returned by our controller
+    public IActionResult Index()
+        {
+            // return a razor view
+            return View();
+        }
+    
     }
 }
