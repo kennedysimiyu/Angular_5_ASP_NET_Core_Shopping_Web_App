387ea94abad9dfc47cd6d357e1d784db740a8fbd
Ken Simiyu <kensimiyu2015@gmail.com>
2018-02-10 12:06:54 +0300

    js form functions

--- SimiyuMall/SimiyuMall/wwwroot/js/index.js
+++ SimiyuMall/SimiyuMall/wwwroot/js/index.js
@@ -1,1 +1,17 @@
-﻿
+﻿var x = 0;
+
+var s = "";
+
+console.log("Hello Customer");
+
+var contactForm = document.getElementById("contactForm");
+    contactForm.hidden = true;
+
+    var button = document.getElementById("buyButton");
+
+    button.addEventListener("click", function () {
+    console.log("Buying Item")
+    });
+
+var productInfo = document.getElementsByClassName(items-props);
+var listItems = productInfo.item[0].children;
