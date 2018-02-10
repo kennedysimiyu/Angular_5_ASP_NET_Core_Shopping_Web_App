8b6ec878297777fb1f8af70efea851106a97445e
Ken Simiyu <kensimiyu2015@gmail.com>
2018-02-10 12:16:35 +0300

    code comments

--- SimiyuMall/SimiyuMall/wwwroot/js/index.js
+++ SimiyuMall/SimiyuMall/wwwroot/js/index.js
@@ -1,17 +1,22 @@
-﻿var x = 0;
+﻿// declared an initialized variables
+var x = 0;
 
 var s = "";
 
+// log information on console when the script is loaded
 console.log("Hello Customer");
 
+// variable storing a functionality for hidding the contactForm
 var contactForm = document.getElementById("contactForm");
     contactForm.hidden = true;
 
+    // variable with a functionality to show on the logs that the buy item button has been clicked.
     var button = document.getElementById("buyButton");
 
     button.addEventListener("click", function () {
     console.log("Buying Item")
     });
 
-var productInfo = document.getElementsByClassName(items-props);
+// product info functionalities.
+var productInfo = document.getElementsByClassName(items - props);
 var listItems = productInfo.item[0].children;
