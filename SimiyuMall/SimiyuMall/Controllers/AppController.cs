2b66296f8a18b0b4328a0df90dae804aa84eec23
Ken Simiyu <kensimiyu2015@gmail.com>
2018-03-22 17:25:42 +0300

    adding the navigation, header and footer code

--- SimiyuMall/SimiyuMall/Controllers/AppController.cs
+++ SimiyuMall/SimiyuMall/Controllers/AppController.cs
@@ -9,12 +9,33 @@ namespace SimiyuMall.Controllers
     // our controller derives from a class in mvc called Controller
     public class AppController : Controller
     {
-    // action returned by our controller
+    // action returned by our controller for Index
     public IActionResult Index()
         {
             // return a razor view
             return View();
         }
     
+    // action result for our contact page
+    public IActionResult Contact()
+        {
+            // setting our contact page title in the controler action class
+            ViewBag.Title = "Contact Us";
+
+            return View();
+
+        }
+
+        // action result for our about page
+        public IActionResult About()
+        {
+            // setting our contact page title in the controler action class
+            ViewBag.Title = "About Us";
+
+            return View();
+
+        }
+
+
     }
 }
